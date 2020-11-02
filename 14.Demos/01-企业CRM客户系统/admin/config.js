1625ec53b20ef3f3045bb17e7b1d8ea2daa5f332
liumengge <785965196@qq.com>
2020-11-02 12:04:34 +0800

    '登录模块开发逻辑'

---
+++ 14.Demos/01-企业CRM客户系统/admin/config.js
@@ -0,0 +1,22 @@
+module.exports = {
+	// => WEB服务端口号，后台服务启动的端口，0~65535之间任何一个没有被占用的端口号即可
+	PORT: 8888,
+
+	//=>CROS跨域相关信息
+	CROS: {
+		ALLOW_ORIGIN: 'http://127.0.0.1:5500',    // 客户端启动服务的地址，表示允许哪一个客户端向当前服务发送请求
+		ALLOW_METHODS: 'PUT,POST,GET,DELETE,OPTIONS,HEAD',
+		HEADERS: 'Content-Type,Content-Length,Authorization, Accept,X-Requested-With',
+		CREDENTIALS: true
+	},
+
+	//=>SESSION存储相关信息
+	SESSION: {
+		secret: 'ZFPX',
+		saveUninitialized: false,
+		resave: false,
+		cookie: {
+			maxAge: 1000 * 60 * 60 * 24 * 30
+		}
+	}
+};
