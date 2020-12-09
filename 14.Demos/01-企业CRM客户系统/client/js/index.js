5eaf5da21ecbd09e2918695e7f2ec2f179275733
Mengge Liu <liumengge_milly@163.com>
2020-12-09 17:41:50 +0800

    首页显示用户名+退出登录实现

--- 14.Demos/01-企业CRM客户系统/client/js/index.js
+++ 14.Demos/01-企业CRM客户系统/client/js/index.js
@@ -1,5 +1,38 @@
 // 3. 基于发布订阅管控获取基本信息之后逐一做的事情
 // 回顾发布订阅模式：03.JavaScript/03-高级/JS设计模式
+let $plan = $.Callbacks();
+
+$plan.add(function (baseInfo) {  
+	// 在头部右上角展示登录用户的基本信息
+	let $baseBox = $('.baseBox'),
+	$baseBoxSpan =  $baseBox.find('span'),
+	$signOut = $baseBox.find('a');
+
+	$baseBoxSpan.html(`您好：${baseInfo.name}`);
+
+	// 退出登录
+	$signOut.click(function () { 
+		alert('您确定要退出登录吗？', {
+			confirm: true,
+			handled: async type => {
+				// 点击确定按钮
+				if (type === 'CONFIRM') {
+					let result = await axios.get('/user/signout');
+					if (parseInt(result.code) !== 0) {
+						alert('退出登录失败，请稍后再试！');
+						return;
+					}
+					alert('退出登录成功，即将退出到登录页面！', {
+						handled: _ => {
+							window.location.href = 'login.html';
+						}
+					});
+				}
+			}
+		})
+	});
+})
+
 
 $(async function () {
 	// 1. 检验当前用户是否登录, 该URL参看API.TXT文件中的相关设置  --- 必做
@@ -48,7 +81,9 @@ $(async function () {
 	// 将用户权限信息添加到用户基本信息对象中
 	results[0]['power'] = results[1];
 	results = results[0];   // 现在的results是用户的基本信息对象
-	console.log(results);
+	// console.log(results);
+	// 通知计划表中的方法执行
+	$plan.fire(results);
 
 });
 
