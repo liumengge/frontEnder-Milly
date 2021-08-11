5e6a1e1f0b8191a4c9bf3faffb9a7d52a2b872b3
liumengge <liumengge_milly@163.com>
2021-08-11 23:58:34 +0800

    模板编译初步

--- 07.Vue框架/Vue2高频手写/1-vue响应式原理/code/src/init.js
+++ 07.Vue框架/Vue2高频手写/1-vue响应式原理/code/src/init.js
@@ -7,5 +7,17 @@ export function initMixin(Vue) {
         vm.$options = options;   // 实例上有个属性$options  表示的是用户传入的所有属性
         // 初始化状态
         initState(vm)
+
+        // 
+        if (vm.$options.el) {  // 表示实例上有el这个属性，数据可以挂载到页面上
+            // 渲染
+            vm.$mount(vm.$options.el)
+        }
+    }
+
+    Vue.prototype.$mount = function (el) {
+        el = document.querySelector(el)
+
+        const vm = this  // 这个this是什么 
     }
 }
