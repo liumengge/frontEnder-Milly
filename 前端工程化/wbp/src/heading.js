d90dc05a5f41d44dc86f6315e83bfb97de71cac4
liumengge <liumengge_milly@163.com>
2021-09-14 00:32:39 +0800

    前端代码构建

---
+++ 前端工程化/wbp/src/heading.js
@@ -0,0 +1,8 @@
+
+
+export default () => {
+  const element = document.createElement('h2')
+  element.textContent = 'Hello webpack'
+  element.addEventListener('click', () => alert('Hello webpack'))
+  return element
+}
