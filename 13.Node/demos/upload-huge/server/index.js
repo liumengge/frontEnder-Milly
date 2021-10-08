a561f189fd3d231c61d50217360e611ace46cd78
liumengge <liumengge_milly@163.com>
2021-10-09 00:30:24 +0800

    大文件上传

--- 13.Node/demos/upload-huge/server/index.js
+++ 13.Node/demos/upload-huge/server/index.js
@@ -1,30 +1,30 @@
-const Controller = require("./controller");
-const http = require("http");
-const server = http.createServer();
+const Controller = require("./controller")
+const http = require("http")
+const server = http.createServer()
 
-const controller = new Controller();
+const controller = new Controller()
 
 server.on("request", async (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Headers", "*");
+  res.setHeader("Access-Control-Allow-Origin", "*")
+  res.setHeader("Access-Control-Allow-Headers", "*")
   if (req.method === "OPTIONS") {
-    res.status = 200;
-    res.end();
-    return;
+    res.status = 200
+    res.end()
+    return
   }
   if (req.url === "/verify") {
-    await controller.handleVerifyUpload(req, res);
-    return;
+    await controller.handleVerifyUpload(req, res)
+    return
   }
 
   if (req.url === "/merge") {
-    await controller.handleMerge(req, res);
-    return;
+    await controller.handleMerge(req, res)
+    return
   }
 
   if (req.url === "/") {
-    await controller.handleFormData(req, res);
+    await controller.handleFormData(req, res)
   }
-});
+})
 
-server.listen(3000, () => console.log("正在监听 3000 端口"));
+server.listen(3000, () => console.log("正在监听 3000 端口"))
