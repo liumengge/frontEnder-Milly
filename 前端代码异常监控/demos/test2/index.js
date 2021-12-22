5d1345a873fd0cbf7ff9474bdebcf6054785a1e4
liumengge <liumengge_milly@163.com>
2021-12-22 14:25:39 +0800

    tracekit分析-未完成

--- 前端代码异常监控/demos/test2/index.js
+++ 前端代码异常监控/demos/test2/index.js
@@ -62,14 +62,68 @@
 //   console.log('catch error', err)
 // })
 
+// debugger
+// async function test_6() {
+//   try {
+//     let response = await fetch('http://no-user-here')
+//     let user = await response.json()
+//   } catch(err) {
+//     // 捕获到 fetch 和 response.json 中的错误
+//     console.log(err)
+//   }
+// }
+// test_6()
 
-async function test() {
-  try {
-    let response = await fetch('/no-user-here')
-    let user = await response.json()
-  } catch(err) {
-    // 捕获到 fetch 和 response.json 中的错误
-    alert(err)
-  }
+// async function test_6() {
+//   let response = await fetch('http://test-url')
+// }
+
+// // test_6() 变成了一个 rejected 的 promise
+// test_6().catch((err) => {
+//   console.log(err)
+// })  // TypeError: failed to fetch
+
+// window.addEventListener('unhandledrejection', function(event) {
+//   // 这个事件对象有两个特殊的属性：
+//   console.log(event.promise); // [object Promise] - 生成该全局 error 的 promise
+//   console.log(event.reason); // Error: Whoops! - 未处理的 error 对象
+// })
+
+window.onunhandledrejection = function(event) {
+  console.log(event)
+  console.log(event.promise)
+  console.log(event.reason)
 }
-test()
+
+window.onerror = function(message, source, line, col, error) {
+  console.log('异常描述信息', message)
+  console.log('出现异常的脚本文件', source)
+  console.log('异常所在文件行列号', line, col)
+  console.log('异常对象', error)
+}
+
+// function test_7() {
+//   test_6()
+// }
+
+// test_7()
+
+// async function test_8() {
+//   let res = await test_9()
+// }
+// test_8()
+
+
+;(async () => {
+  try {
+    // test_7()
+    let res = await fetch('http://test-url')
+  } catch (error) {
+    if (error instanceof ReferenceError) {
+      console.log('ReferenceError, do something here')
+    } else {
+      // 其他类型的错误不知道怎么处理，抛出
+      throw new Error(`出现一个不知该如何处理的错误：${error}`)
+    }
+  }
+})()
