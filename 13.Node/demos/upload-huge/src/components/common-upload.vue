83c945de7e57e3ad9f51bd9c76d5d4cb4d3845b6
liumengge <liumengge01@corp.netease.com>
2022-01-13 09:46:39 +0800

    公共s上传组件封装

---
+++ 13.Node/demos/upload-huge/src/components/common-upload.vue
@@ -0,0 +1,46 @@
+<template>
+  <el-input v-model="fileName" @update="onInput">
+    <template #prepend>
+      <el-button icon="el-icon-upload" type="primary" style="font-size: 18px" @click="onClick"></el-button>
+      <input type="file" ref="file" style="position: fixed; left: -1px; right: -1px; width: 1px; height: 1px;" @change="onChange">
+    </template>
+  </el-input>
+</template>
+
+<script>
+export default {
+  props: {
+    url: String,       // 文件上传地址
+    extends: String,   // 文件类型(文件拓展名)
+    fileName: String,   // 上传文件名称
+
+  },
+  setup() {
+    const file = ref('')
+
+    function upload(url, data) {
+      return process(fetch(url, {
+        method: 'POST',
+        body: data
+      }), url)
+    }
+
+
+    return {
+      onInput() {
+
+      },
+      onClick() {
+
+      },
+      onChange() {
+
+      }
+    }
+  }
+}
+</script>
+
+<style scoped lang="less">
+
+</style>
