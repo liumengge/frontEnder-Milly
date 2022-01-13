4220cf1aa63a9b66ee5d9d87949d2caf35520b6a
liumengge <liumengge01@corp.netease.com>
2022-01-13 23:35:43 +0800

    css动画

--- 02.CSS/CSS动画/demo-1/dist/index.js
+++ 02.CSS/CSS动画/demo-1/dist/index.js
@@ -1,1 +1,32 @@
+function initializeWaterMark(text) {
+  function render() {
+    if (!text) return
+    $('#water-mark').empty();
+    var top = 50;
+    var win_width = window.innerWidth;   // 窗口内部宽度
+    var win_height = window.innerHeight;
+    var itemWidth = 0;
+    var itemHeight = 0;
+    var itemHeightAbsolute = 0; // 考虑旋转情况下的真实高度
+    while (1) {
+      var left = 30;
+      while (1) {
+        var div = document.createElement('div');
+        $(div).addClass('printItem').text(text).css({top: top + 'px', left: left + 'px'}).appendTo('#water-mark');
+        left += 200;
+        if (itemWidth === 0) {  // 每个 div 大小相同，避免多次计算
+          itemWidth = div.offsetWidth;   // 包含内边距padding和border宽度
+          itemHeight = div.offsetHeight;
+          itemHeightAbsolute = Math.sqrt(itemWidth * itemWidth * 0.25 + itemHeight * itemHeight * 0.25);
+        }
+        if (itemWidth + left >= win_width) break // 超出屏幕宽度
+      }
+      top += 150;
+      if (top + itemHeightAbsolute >= win_height) break
+    }
+  }
+  $(window).on('resize', render);
+  render();
+}
 
+initializeWaterMark('Milly');
